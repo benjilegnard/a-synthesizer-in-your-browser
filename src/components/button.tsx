06efdab116ccf96901545f4355fb8a8f0f9d2fbb
Benjamin Legrand <benjilegnard@protonmail.com>
2025-02-24 20:11:28 +0100

    feat: add click handler to button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,14 +3,18 @@ import { FunctionalComponent } from "preact";
 
 const { colors } = flavors.mocha;
 
-export const Button: FunctionalComponent = (props) => (
-  <button
-    style={{
-      border: `solid 2px ${colors.teal.hex}`,
-      padding: "24px",
-      background: colors.surface0.hex,
-    }}
-  >
-    {props.children}
-  </button>
+interface ButtonProps {
+    onClick: (event: MouseEvent) => void;
+}
+export const Button: FunctionalComponent<ButtonProps> = (props) => (
+    <button
+        style={{
+            border: `solid 2px ${colors.teal.hex}`,
+            padding: "24px",
+            background: colors.surface0.hex,
+
+        }}
+    >
+        {props.children}
+    </button>
 );
